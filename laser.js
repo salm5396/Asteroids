cb0add213636efaacc9eabc2ca6b7e23ebb1fbff
salm5396 <48219319+salm5396@users.noreply.github.com>
2019-05-02 18:39:33 -0500

    Update laser.js

--- laser.js
+++ laser.js
@@ -14,7 +14,16 @@ function Laser(spos, angle) {
         point(this.pos.x, this.pos.y);
         pop();
     }
-    
+
+    this.hits = function (asteroid) {
+        var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+        if (d < asteroid.r) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     this.offscreen = function () {
         if (this.pos.x > width || this.pos.x < 0) {
             return true;
