ad02a6d17aa42ed8194a21efc8706b639473f03c
salm5396 <48219319+salm5396@users.noreply.github.com>
2019-05-02 18:38:49 -0500

    added asteroids
    added asteroid functionality

--- sketch.js
+++ sketch.js
@@ -1,53 +1,83 @@
 var ship;
+var asteroids = [];
 var lasers = [];
 
-//function to create the space for our game
-//also creates the ship and will initialize asteroids (TODO).
 function setup() {
     createCanvas(windowWidth, windowHeight);
     ship = new Ship();
+    for (var i = 0; i < 5; i++) {
+        asteroids.push(new Asteroid());
+    }
 }
 
 function draw() {
     background(0);
 
-    //big loop for lasers going off screen
-    for (var i = lasers.length - 1; i >= 0; i--) {
+    for (var i = 0; i < asteroids.length; i++) {
+        if (ship.hits(asteroids[i])) {
+            console.log('crash!');
+        }
+        asteroids[i].render();
+        asteroids[i].update();
+        asteroids[i].edges();
+    }
+
+
+    //outer loop for rendering lasers
+    for (var i = lasers.length - 1; i >= 0; i--)
+    {
         lasers[i].render();
         lasers[i].update();
+
+        //if the laser goes off the screen, send it through the opposite side
         if (lasers[i].offscreen()) {
             lasers.splice(i, 1);
-        } 
+        }
+        else
+        {
+            //loop to check each laser if it has hit an asteroid
+            for (var j = asteroids.length - 1; j >= 0; j--)
+            {
+                if (lasers[i].hits(asteroids[j])) {
+                    //if the size of the asteroid is over 20, then break it in two
+                    if (asteroids[j].r > 20)
+                    {
+                        var newAsteroids = asteroids[j].breakup();
+                        asteroids = asteroids.concat(newAsteroids);
+                    }
+                    asteroids.splice(j, 1);
+                    lasers.splice(i, 1);
+                    break;
+                }
+            }
+        }
     }
-
-    //the stuff for moving spaceship or shooting lasers
-    if (keyIsDown(38)) { //up arrow
-        ship.thrusting(true);
-    }
-    if (keyIsDown(37)) { //left arrow
-        ship.setRotation(-0.1);
-    }
-    if (keyIsDown(39)) { //right arrow
-        ship.setRotation(0.1);
-    }
-
-    //updates everything needed with the ship
     ship.render();
     ship.turn();
     ship.update();
     ship.edges();
 
 
+    //stuff so that continuous pressing of buttons is okay
+    if (keyIsDown(RIGHT_ARROW)) {
+        ship.setRotation(0.1);
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        ship.setRotation(-0.1);
+    }
+    if (keyIsDown(UP_ARROW)) {
+        ship.thrusting(true);
+    }
 }
 
-//when the specified key is released, stop doing that thing
+//stops thrusting if the up button is released
 function keyReleased() {
     ship.setRotation(0);
     ship.thrusting(false);
 }
 
 function keyPressed() {
-    if (key == " ") { // spacebar
-        lasers.push(new Laser(ship.pos, ship.heading));
-    }
+        if (key == ' ') {
+            lasers.push(new Laser(ship.pos, ship.heading));
+        }
 }
