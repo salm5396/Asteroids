68fcdd709b027f613af2a01fae4d15f1476bd6e2
salm5396 <48219319+salm5396@users.noreply.github.com>
2019-05-04 14:43:24 -0500

    added crash functionality

--- ship.js
+++ ship.js
@@ -5,9 +5,17 @@ function Ship() {
     this.rotation = 0;
     this.vel = createVector(0, 0);
     this.isThrusting = false;
+    this.isDestroyed = false;
+    this.destroyFrames = 600;
 
     this.thrusting = function (b) {
         this.isThrusting = b;
+    }
+
+    this.thrust = function () {
+        var force = p5.Vector.fromAngle(this.heading);
+        force.mult(0.1);
+        this.vel.add(force);
     }
 
     this.update = function () {
@@ -15,13 +23,28 @@ function Ship() {
             this.thrust();
         }
         this.pos.add(this.vel);
-        this.vel.mult(0.99);
+        if (this.isDestroyed) {
+            for (var i = 0; i < this.brokenParts.length; i++) {
+                this.brokenParts[i].pos.add(this.brokenParts[i].vel);
+                this.brokenParts[i].heading += this.brokenParts[i].rot;
+            }
+        }
+        else {
+            this.vel.mult(0.99);
+        }
+        
     }
 
-    this.thrust = function () {
-        var force = p5.Vector.fromAngle(this.heading);
-        force.mult(0.1);
-        this.vel.add(force);
+    this.brokenParts = [];
+    this.destroy = function () {
+        this.isDestroyed = true;
+        for (var i = 0; i < 4; i++)
+            this.brokenParts[i] = {
+                pos: this.pos.copy(),
+                vel: p5.Vector.random2D(),
+                heading: random(0, 360),
+                rot: random(-0.07, 0.07)
+            };
     }
 
     this.hits = function (asteroid) {
@@ -34,13 +57,26 @@ function Ship() {
     }
 
     this.render = function () {
-        push();
-        translate(this.pos.x, this.pos.y);
-        rotate(this.heading + PI / 2);
-        fill(0);
-        stroke(255);
-        triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
-        pop();
+        if (this.isDestroyed) {
+            for (var i = 0; i < this.brokenParts.length; i++) {
+                push();
+                stroke(floor(255 * ((this.destroyFrames--) / 600)));
+                var bp = this.brokenParts[i];
+                translate(bp.pos.x, bp.pos.y);
+                rotate(bp.heading);
+                line(-this.r / 2, -this.r / 2, this.r / 2, this.r / 2);
+                pop();
+            }
+        }
+        else {
+            push();
+            translate(this.pos.x, this.pos.y);
+            rotate(this.heading + PI / 2);
+            fill(0);
+            stroke(255);
+            triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+            pop();
+        }  
     }
 
     this.edges = function () {
