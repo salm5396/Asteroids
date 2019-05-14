60773d18a6c862c40bc1da503504682beecbe354
vinhngu <48768220+vinhngu@users.noreply.github.com>
2019-05-14 15:08:43 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var sqlite3 = require('sqlite3');
 var app = express();
 var port = 8004;
 
-var db_filename = path.join(__dirname, 'db', 'imdb.sqlite3');
+var db_filename = path.join(__dirname, 'db', 'userdata.sqlite3');
 var public_dir = path.join(__dirname, 'public');
 
 var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
@@ -19,64 +19,6 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
 });
 
 app.use(express.static(public_dir));
-
-app.get('/Titles', (req, res) => {
-    var req_url = url.parse(req.url);
-    var query = decodeURI(req_url.query).replace(/\*/g, '%');
-    db.all('SELECT * FROM Titles WHERE primary_title LIKE ?', [query], (err, rows) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.write(JSON.stringify(rows));
-            res.end();
-        }
-    });
-});
-
-app.get('/Names', (req, res) => {
-    var req_url = url.parse(req.url);
-    var query = decodeURI(req_url.query).replace(/\*/g, '%');
-    db.all('SELECT * FROM Names WHERE primary_name LIKE ?', [query], (err, rows) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.write(JSON.stringify(rows));
-            res.end();
-        }
-    });
-});
-
-app.get('/titles/:tconst', (req, res) => {
-    console.log(req.params);
-    db.get('SELECT * FROM Titles WHERE tconst = ?', [req.params.tconst], (err, row) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.write(JSON.stringify(row));
-            res.end();
-        }
-    });
-});
-
-app.get('/names/:nconst', (req, res) => {
-    console.log(req.params);
-    db.get('SELECT * FROM Names WHERE nconst = ?', [req.params.nconst], (err, row) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.write(JSON.stringify(row));
-            res.end();
-        }
-    });
-});
-
+    
 var server = app.listen(port);
 
